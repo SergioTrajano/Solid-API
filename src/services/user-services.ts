cf5a7445d474e3d9bcf93ce209d8ce5554046cb7
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-08-30 22:14:18 -0300

    refactor: :recycle: Use repository to create user in service

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -1,11 +1,15 @@
 import { prisma } from "@/lib/prisma";
 import { hash } from "bcryptjs";
 
+import { PrismaUserRepositories } from "@/repositories/prisma-users-repositories";
+
 interface RegisterParams {
     email: string;
     name: string;
     password: string;
 }
+
+const prismaUserRepositories = new PrismaUserRepositories();
 
 export async function userRegister({ email, name, password }: RegisterParams) {
     const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
@@ -16,11 +20,5 @@ export async function userRegister({ email, name, password }: RegisterParams) {
 
     const passwordHash = await hash(password, 6);
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: passwordHash,
-        },
-    });
+    await prismaUserRepositories.create({ email, name, password_hash: passwordHash });
 }
