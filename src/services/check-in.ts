196a8ad2ec457e7d2dcf7b4e12294190820312ac
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 16:23:48 -0300

    feat: :sparkles: Validate if user is nerby gym to check in

--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from "@prisma/client";
 
 import { CheckInRepository } from "@/repositories/check-ins-repository";
 import { GymRepository } from "@/repositories/gym-repository";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { ResourceNotFoundError } from "./errors/resource-not-found";
 
 interface CheckInServiceRequest {
@@ -21,11 +22,26 @@ export class CheckInService {
         private gymRepository: GymRepository
     ) {}
 
-    async execute({ gymId, userId }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    async execute({
+        gymId,
+        userId,
+        userLatitude,
+        userLongitude,
+    }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
         const gym = await this.gymRepository.findById(gymId);
 
         if (!gym) {
             throw new ResourceNotFoundError();
+        }
+
+        const distanceBetweenUserAndGym = getDistanceBetweenCoordinates(
+            { latitude: userLatitude, longitude: userLongitude },
+            { latitude: Number(gym?.latitude), longitude: Number(gym?.longitude) }
+        );
+        const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+        if (distanceBetweenUserAndGym > MAX_DISTANCE_IN_KILOMETERS) {
+            throw new Error();
         }
 
         const checkInOnSameDate = await this.checkInRepository.findByUserIdOnDate(
