c06c494ad1df743375e11d13158f15bacf15d5e6
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 17:49:00 -0300

    test: :white_check_mark: Test if is possible to get user check-in history

---
+++ src/services/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,38 @@
+import { beforeEach, describe, expect, test } from "vitest";
+
+import { InMemoryCheckInRepository } from "@/repositories/in-memory/in-memory-check-in-repository";
+
+import { FetchUserCheckInsHistoryService } from "./fetch-user-check-ins-history";
+
+let checkInRepository: InMemoryCheckInRepository;
+let sut: FetchUserCheckInsHistoryService;
+
+describe("Check in service", () => {
+    beforeEach(() => {
+        checkInRepository = new InMemoryCheckInRepository();
+        sut = new FetchUserCheckInsHistoryService(checkInRepository);
+    });
+
+    test("should be able to fetch check-in history", async () => {
+        await checkInRepository.create({
+            gym_id: "gym-1",
+            user_id: "user-1",
+        });
+        await checkInRepository.create({
+            gym_id: "gym-2",
+            user_id: "user-1",
+        });
+
+        const { checkIns } = await sut.execute({ userId: "user-1", page: 1 });
+
+        expect(checkIns).toHaveLength(2);
+        expect(checkIns).toEqual([
+            expect.objectContaining({
+                gym_id: "gym-1",
+            }),
+            expect.objectContaining({
+                gym_id: "gym-2",
+            }),
+        ]);
+    });
+});
