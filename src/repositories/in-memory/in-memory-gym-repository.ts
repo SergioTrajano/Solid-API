0ebc29c706b90152b9e26b011ccb1b5ff7c38cf8
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 16:58:29 -0300

    feat: :card_file_box: Add in memory gym database

--- src/repositories/in-memory/in-memory-gym-repository.ts
+++ src/repositories/in-memory/in-memory-gym-repository.ts
@@ -1,9 +1,26 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
 
+import { Decimal } from "@prisma/client/runtime/library";
+import { randomUUID } from "node:crypto";
 import { GymRepository } from "../gym-repository";
 
 export class InMemoryGymRepository implements GymRepository {
     public gyms: Gym[] = [];
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: data.id ?? randomUUID(),
+            description: data.description ?? null,
+            phoneNumber: data.phoneNumber ?? "",
+            title: data.title,
+            latitude: new Decimal(String(data.latitude)),
+            longitude: new Decimal(String(data.longitude)),
+        };
+
+        this.gyms.push(gym);
+
+        return gym;
+    }
 
     async findById(id: string) {
         const gym = this.gyms.find((gym) => gym.id === id);
