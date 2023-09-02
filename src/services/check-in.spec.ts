e0af3fefc7678e3418ffdfa3352636fb8caffc5d
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 15:22:40 -0300

    test: :white_check_mark: Test if is not possible to check in twice in same date

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -26,4 +26,20 @@ describe("Check in service", () => {
 
         expect(checkIn.id).toEqual(expect.any(String));
     });
+
+    test("should not be able to check in twice in the same day", async () => {
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+        await sut.execute({
+            gymId: "gym-01",
+            userId: "user-01",
+        });
+
+        await expect(() =>
+            sut.execute({
+                gymId: "gym-01",
+                userId: "user-01",
+            })
+        ).rejects.toBeInstanceOf(Error);
+    });
 });
