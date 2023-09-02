a820f93e9a59c70d5d4fdce579fa640edf254274
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 14:50:48 -0300

    feat: :sparkles: Generate a random uuid for users in in memory user repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,4 +1,6 @@
 import { Prisma, User } from "@prisma/client";
+import { randomUUID } from "node:crypto";
+
 import { UserRepository } from "../user-repository";
 
 export class InMemoryUsersRepository implements UserRepository {
@@ -6,7 +8,7 @@ export class InMemoryUsersRepository implements UserRepository {
 
     async create(data: Prisma.UserCreateInput) {
         const user = {
-            id: "user-1",
+            id: randomUUID(),
             name: data.name,
             email: data.email,
             password_hash: data.password_hash,
