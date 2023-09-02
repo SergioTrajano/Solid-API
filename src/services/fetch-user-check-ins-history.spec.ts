6cfe77b97db67750a5d874ddc3fa0ff080044e9e
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 17:49:31 -0300

    test: :white_check_mark: Test if check-in history is paginated

--- src/services/fetch-user-check-ins-history.spec.ts
+++ src/services/fetch-user-check-ins-history.spec.ts
@@ -35,4 +35,25 @@ describe("Check in service", () => {
             }),
         ]);
     });
+
+    test("should be able to fetch paginated check-in history", async () => {
+        for (let i = 1; i <= 22; i++) {
+            await checkInRepository.create({
+                gym_id: `gym-${i}`,
+                user_id: "user-1",
+            });
+        }
+
+        const { checkIns } = await sut.execute({ userId: "user-1", page: 2 });
+
+        expect(checkIns).toHaveLength(2);
+        expect(checkIns).toEqual([
+            expect.objectContaining({
+                gym_id: "gym-21",
+            }),
+            expect.objectContaining({
+                gym_id: "gym-22",
+            }),
+        ]);
+    });
 });
