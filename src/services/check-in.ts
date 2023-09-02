3938c7226cf25d70d8853421b60447d7c8d00869
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-02 15:21:17 -0300

    feat: :sparkles: Add condition to only check in if user did not checked in already

--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -15,6 +15,15 @@ export class CheckInService {
     constructor(private checkInRepository: CheckInRepository) {}
 
     async execute({ gymId, userId }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+        const checkInOnSameDate = await this.checkInRepository.findByUserIdOnDate(
+            userId,
+            new Date()
+        );
+
+        if (checkInOnSameDate) {
+            throw new Error();
+        }
+
         const checkIn = await this.checkInRepository.create({ gym_id: gymId, user_id: userId });
 
         return { checkIn };
