5ce86d1a8b7a17cafbbdf59edf59ef0bfdc276d2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-03 16:18:13 -0300

    feat: :label: Update checkIn repository interface

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,7 @@ export interface CheckInRepository {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+    findById(id: string): Promise<CheckIn | null>;
     countByUserId(userId: string): Promise<number>;
+    save(checkin: CheckIn): Promise<CheckIn>;
 }
