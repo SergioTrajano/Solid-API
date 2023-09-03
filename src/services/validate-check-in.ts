fd85fa30d15a7a9cdf690281a55e126e722a242a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-03 17:01:02 -0300

    feat: :goal_net: Verify if check-in validation is within 20min of its creation

--- src/services/validate-check-in.ts
+++ src/services/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from "@prisma/client";
 
 import { CheckInRepository } from "@/repositories/check-ins-repository";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 import { ResourceNotFoundError } from "./errors/resource-not-found";
 
 interface ValidateCheckInServiceRequest {
@@ -21,6 +23,20 @@ export class ValidateCheckInService {
 
         if (!checkIn) {
             throw new ResourceNotFoundError();
+        }
+
+        const differenceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            "minutes"
+        );
+
+        const MAX_DIFFERENCE_IN_MINUTES_BETWEEN_CHECK_IN_CREATION_AND_VALIDATION_TIME = 20;
+
+        if (
+            differenceInMinutesFromCheckInCreation >
+            MAX_DIFFERENCE_IN_MINUTES_BETWEEN_CHECK_IN_CREATION_AND_VALIDATION_TIME
+        ) {
+            throw new LateCheckInValidationError();
         }
 
         checkIn.validated_at = new Date();
