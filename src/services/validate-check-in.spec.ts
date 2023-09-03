9799aa49d969d5c3b0ed92597470746c6ddcd1f7
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-09-03 16:37:57 -0300

    test: :white_check_mark: Test check-in validation

---
+++ src/services/validate-check-in.spec.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, test } from "vitest";
+
+import { InMemoryCheckInRepository } from "@/repositories/in-memory/in-memory-check-in-repository";
+import { ValidateCheckInService } from "./validate-check-in";
+
+let checkInRepository: InMemoryCheckInRepository;
+let sut: ValidateCheckInService;
+
+describe("Validate check-in service", () => {
+    beforeEach(() => {
+        checkInRepository = new InMemoryCheckInRepository();
+        sut = new ValidateCheckInService(checkInRepository);
+    });
+
+    test("should be able to validate check-in", async () => {
+        const createdCheckIn = await checkInRepository.create({
+            gym_id: "gym-1",
+            user_id: "user-1",
+        });
+
+        const { checkIn: validatedCheckIn } = await sut.execute({
+            checkInId: createdCheckIn.id,
+        });
+
+        expect(validatedCheckIn.validated_at).toEqual(expect.any(Date));
+    });
+});
