ab583e7f4b5d17f45a6c85052d45f56125000335
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-10-06 00:52:59 -0300

    refactor: :recycle: Update profile router creation to use auth middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
 
+import { verifyJWT } from "@/middlewares/verify-jwt";
+
 import { authenticate } from "./controllers/authenticate-controller";
 import { profile } from "./controllers/profile";
 import { createUser } from "./controllers/users-controller";
@@ -8,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
     app.post("/users", createUser);
     app.post("/sessions", authenticate);
 
-    app.get("/me", profile);
+    app.get("/me", { onRequest: verifyJWT }, profile);
 }
