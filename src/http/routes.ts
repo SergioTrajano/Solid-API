752c84e3c0b5b58335b72455f15d0b05952128bf
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-10-06 00:34:02 -0300

    feat: :sparkles: Add profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
 
 import { authenticate } from "./controllers/authenticate-controller";
+import { profile } from "./controllers/profile";
 import { createUser } from "./controllers/users-controller";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/users", createUser);
     app.post("/sessions", authenticate);
+
+    app.get("/me", profile);
 }
