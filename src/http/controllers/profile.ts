6a429a6a77a3c0b2c5fb71ca91e9da6266e6ae7a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-10-06 00:52:29 -0300

    feat: :sparkles: UPdate profile route to send user data

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,13 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
+import { makeGetUserProfileService } from "@/services/factories/make-get-user-profile-service";
+
 export async function profile(req: FastifyRequest, res: FastifyReply) {
-    await req.jwtVerify();
+    const getUserProfile = makeGetUserProfileService();
 
-    console.log(req.user.sub);
+    const { user } = await getUserProfile.execute({ userId: req.user.sub });
 
-    return res.status(200).send();
+    return res.status(200).send({
+        user: { ...user, password_hash: undefined },
+    });
 }
