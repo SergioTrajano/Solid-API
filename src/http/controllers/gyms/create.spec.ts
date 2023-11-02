f953605812ca49356d4d23b7053fde694b32c079
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-11-01 22:49:36 -0300

    test: :white_check_mark: Add test to create gym controller

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create gym (e2e)", () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    test("Should be able to create a gym", async () => {
+        const { token } = await createAndAuthenticateUser(app);
+
+        const response = await request(app.server)
+            .post("/gyms")
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                title: "JS Gym",
+                description: "Lore ypsilum",
+                phone: "999999999",
+                latitude: -11.29851,
+                longitude: -14.66197,
+            });
+
+        expect(response.statusCode).toEqual(201);
+    });
+});
