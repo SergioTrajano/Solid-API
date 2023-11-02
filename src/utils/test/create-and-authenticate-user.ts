cd52da3ada33b7b4a54cb3125801034491e4d835
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-11-01 22:49:18 -0300

    feat: :sparkles: Add function to create and authenticate user

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from "fastify";
+import request from "supertest";
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+    await request(app.server)
+        .post("/users")
+        .send({ name: "Jhon doe", email: "johndoe@example.com", password: "12345678" });
+
+    const authResponse = await request(app.server).post("/sessions").send({
+        email: "johndoe@example.com",
+        password: "12345678",
+    });
+
+    const { token } = authResponse.body;
+
+    return { token };
+}
